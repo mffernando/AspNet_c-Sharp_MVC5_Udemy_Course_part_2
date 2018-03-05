5dfb86ade09aef21b87c7b52afd2f8f2a797a2fe
Fernando <fmti91@hotmail.com>
2018-03-05 08:06:48 -0300

    Class 39 and 40:  Constructor HomeController.cs

--- Routes/Controllers/HomeController.cs
+++ Routes/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Routes.Models;
 
 namespace Routes.Controllers
 {
     public class HomeController : Controller
     {
+        //Notice from Models > Notice.cs
+        private readonly IEnumerable<Notice> allNotices;
+
+        //constructor
+        public HomeController()
+        {
+            //order by descending date (x=>x.Date)
+            allNotices = new Notice().AllNotices().OrderByDescending(x=>x.Date);
+        }
+
         public ActionResult Index()
         {
-            return View();
+            //last 3 notices
+            var lastNotices = allNotices.Take(3);
+            //all categories distinct
+            var allCategories = allNotices.Select(x=>x.Category).Distinct().ToList();
+            ViewBag.Categories = allCategories;
+            //return last notices typed view
+            return View(lastNotices);
         }
 
         public ActionResult About()
---
+++ Routes/Models/Notice.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+
+namespace Routes.Models
+{
+    public class Notice
+    {
+        public int NoticeId { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public IEnumerable<Notice> AllNotices()
+        {
+            var return = new Collection<Notice>
+            {
+                new Notice
+                {
+                    NoticeId=1,
+                    Category="Soccer",
+                    Title="Soccer game",
+                    Content="Notice about soccer games!",
+                    Date=new Datetime(2018,3,5),
+                },
+                new Notice
+                {
+                    NoticeId=2,
+                    Category="Basketball",
+                    Title="Basketball game",
+                    Content="Notice about Basktetball games!",
+                    Date=new Datetime(2018,3,5),
+                },
+                new Notice
+                {
+                    NoticeId=3,
+                    Category="Volleyball",
+                    Title="Volleyball game",
+                    Content="Notice about Volleyball games!",
+                    Date=new Datetime(2018,3,5),
+                },
+            }
+        }
+    }
+}
