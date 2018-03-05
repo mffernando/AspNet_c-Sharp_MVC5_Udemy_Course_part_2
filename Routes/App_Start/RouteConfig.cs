3fc00771841141a7f689a83918964c00e15dc158
Fernando <fmti91@hotmail.com>
2018-03-05 14:29:44 -0300

    Class 47 and 48: Route configuration (RouteConfig.cs) urls.

--- Routes/App_Start/RouteConfig.cs
+++ Routes/App_Start/RouteConfig.cs
@@ -13,6 +13,28 @@ namespace Routes
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // localhost/news
+            routes.MapRoute(
+                name: "News",
+                url:"news/",
+                defaults: new { controller = "Home", action = "News"}
+                );
+
+            //category in HomeController public ActionResult ShowCategory(string category)
+            // localhost/news/category
+            routes.MapRoute(
+                name: "Show specific category",
+                url:"news/{category}/",
+                defaults: new { controller = "Home", action = "ShowCategory"}
+                );
+
+            // localhost/news/category/new
+            routes.MapRoute(
+                name: "News name",
+                url: "news/{category}/{title}-{noticeId}/",
+                defaults: new { controller = "Home", action = "ShowNew" }
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
